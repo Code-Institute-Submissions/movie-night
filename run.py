8cd0c71391b1f9ab9e44a8e04e3ac6325d9d12cb
prakash joshi <prakash.joshi1402@icloud.com>
2024-04-01 13:47:18 +0000

    Imported libraries and started writing scrapmyweb function. part1:check for exisitng html file in the working directory and give option to view em

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import requests
+from bs4 import BeautifulSoup
+import os
+
+def scrapMyWeb(web_address):
+    try:
+        current_directory=os.getcwd()
+        html_files=[]
+        for filename in os.listdir():
+            if filename.endswith(".html"):
+                html_files.append(filename)
+        if html_files:
+            view_existing_files=input("One or more existing (.html)files are found. Do you wish to see them ('y/n'): ")
+            if view_existing_files.lower()!='n':
+                print("Existing HTML files in the current directory are:\n")
+                for index,exisitng_files in enumerate(html_files):
+                    print(f"{index+1}.{existing_files}")
+                user_choice=input("\nUse existing file (enter number) or create new (enter 'n'): ")
+                if user_choice.lower()!='n':
+                    file_path=os.path.join(current_directory,html_files[int(user_choice)-1])
+                    return
+                else:
+                    save_location = input("Enter the desired filename: \n")  
+                    if not save_location.endswith(".html"):
+                        save_location += ".html" 
+    
+
+    except requests.exceptions.RequestException as shownerror:
+        print("Error downloading webpage:",shownerror)
+    except (FileNotFoundError, IOError) as shownerror:
+        print("Error handling file:",shownerror)
+
